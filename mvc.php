f807adcd68cd91db19d69e43c307dc7a1b3d2617
Tphper <532145595@qq.com>
2015-05-20 16:14:43 +0800

    Update mvc.php

--- mvc.php
+++ mvc.php
@@ -1,1 +1,14 @@
 mvc
+<?php
+function ip_address() {
+	if(!empty($_SERVER["HTTP_CLIENT_IP"])) {
+		$ip_address = $_SERVER["HTTP_CLIENT_IP"];
+	}else if(!empty($_SERVER["HTTP_X_FORWARDED_FOR"])){
+		$ip_address = array_pop(explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']));
+	}else if(!empty($_SERVER["REMOTE_ADDR"])){
+		$ip_address = $_SERVER["REMOTE_ADDR"];
+	}else{
+		$ip_address = '';
+	}
+	return $ip_address;
+}
